2508e399588bfd16cfe194cce68efd378cd018d2
Your Name <you@example.com>
2020-07-30 13:53:01 -0400

    now with moar internets

--- poller.py
+++ poller.py
@@ -4,37 +4,19 @@
 import subprocess
 import shlex
 import os.path
+
+# env var
+environment = 'DEV'
+
+# user vars
 catalina = ('/Applications/Install macOS Catalina.app/Contents/Resources/startosinstall', '--agreetolicense', '--forcequitapps')
 safari = ('open', '-a', 'Safari.app')
 catalina_lt_pkg = ('jamf', 'policy', '-event', 'install-catalina-lt')
 ls = ('ls', '-ltar')
 pwd = ('pwd')
-def check_for_installer(cmd):
-    if os.path.isfile(cmd):
-        print('installer found')
-        fire_window(popup_a)
-        return True
-    else:
-        print('installer not found, downloading JSS')
-        fire_window(popup_b)
-        run_command(catalina_lt_pkg)
-        return False
-def run_command(cmd):
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+pmset = shlex.split('pmset -g ac')
 
-    while True:
-        output = process.stdout.readline()
-        if output == '' and process.poll() is not None:
-            break   
-        if output: 
-            print output.strip()
-    rc = process.poll()
-    return rc
-
-def fire_window(cmd): #feed me a build_window()
-
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
+# windows
 popup_a = (
     '/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper',
     '-windowType',
@@ -69,9 +51,73 @@ popup_b = (
     '-button1',
     'Just my luck...'
 )
+plug_in = (
+    '/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper',
+    '-windowType',
+    'hud',
+    'You are on battery',
+    '-title',
+    'please wait...',
+    '-description',
+    'Nice try but you need to have your charger plugged in, go find it and rerun this program please',
+    '-icon',
+    '/System/Library/CoreServices/Problem Reporter.app/Contents/Resources/ProblemReporter.icns',
+    '-defaultbutton',
+    '1',
+    '-button1',
+    'Finding Charger'
+)
+def check_for_installer(cmd):
+    if os.path.isfile(cmd):
+        fire_window(popup_a)
+        return True
+    else:
+        print('installer not found, downloading JSS')
+        fire_window(popup_b)
+        run_command(catalina_lt_pkg)
+        return False
+
+def check_for_battery(cmd):
+    if 'No adapter attached.' in run_regular(cmd):
+        print('battery user')
+    else:
+        return True
+
+
+def run_regular(cmd):
+    data = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout,stderr = data.communicate()
+    return stdout
+
+def run_command(cmd):
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break   
+        if output: 
+            print output.strip()
+    rc = process.poll()
+    return rc
+
+
+def fire_window(cmd): #feed me a build_window()
+
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
 def main():
-    if check_for_installer(catalina[0]):
-        run_command(catalina)
-    
+    #if check_for_installer(catalina[0]) and check_for_battery(pmset):
+    if check_for_battery(pmset):
+        if check_for_installer(catalina[0]):
+            if environment != 'DEV':
+                run_command(catalina)
+            else:
+                print('dev mode not making any changes')
+    else:
+        fire_window(plug_in)
+
+
 if __name__ == "__main__":
-    main()           
+    main()
