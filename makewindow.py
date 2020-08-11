72c31678df7aa866deb62b34fd69448372f828b3
Your Name <you@example.com>
2020-08-11 12:04:30 -0400

    using makewindow class

--- makewindow.py
+++ makewindow.py
@@ -2,39 +2,31 @@
 # windows
 class Make_Window():
 
-    def __init__(self):
+    def __init__(self,bar,content,button):
         self.window = { 
             'app': '''/Library/Application Support/JAMF/bin/\
 jamfHelper.app/Contents/MacOS/jamfHelper''',
             'cmd1': '-windowType',
             'type': 'hud',
-            'bar': 'update in progress',
             'cmd2':   '-title',
-            'title': 'please wait...',
+            'title': bar,
             'cmd3': '-description',
-            'content': '''We need you to hold on because a the installer is running.
-
-                Connect your changer and Work at your own risk a reboot is immintent.''',
+            'content': content,
             'cmd4': '-icon',
             'icon': '''/System/Library/CoreServices/Problem Reporter.app/Contents/Resources/ProblemReporter.icns''',
             'cmd5': '-defaultbutton',
             'default_btn':'1',
             'cmd6': '-button1',
-            'button': 'Great'
+            'button': button
         } 
 
-    def list_it(self):
-        windowz = []
+    def create(self):
+        '''internal function'''
+        self.windowz = []
         #print(self.window['app'])
         for k, v in self.window.items():
             #print(k, v)
             #windowz.append(k)
-            windowz.append(v)
-        print(f"windowz is {windowz}")
-        return windowz
-
-
-    def make_list(self, bar, content, button):
-        self.window['bar'] = bar
-        self.window['content'] = content
-        self.window['button'] = button
+            self.windowz.append(v)
+        print(f"windowz is {self.windowz}")
+        return self.windowz
